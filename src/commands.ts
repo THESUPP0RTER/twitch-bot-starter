4e36f54f8ce50f567f9c2b6d85981c47362480ed
Hunter Lewis <thesupprter@gmail.com>
2025-04-04 18:28:20 +0000

    added command register function + basic logging

--- src/commands.ts
+++ src/commands.ts
@@ -4,3 +4,69 @@
 //We need a register function will take in the command, and the handler function, and some optional options
 //We need permissions just to see if they're the broadcaster, mod, sub, vip, etc, or specific users
 //Getters
+
+
+export interface CommandOptions {
+    description?: string;
+    requiredPermissions?: Permission[];
+    approvedUsers?: string[];
+    notifyDenial?: boolean;
+    usage?: string;
+    timeLimit?: number;
+
+}
+
+export interface Command {
+    handler: CommandHandler;
+    options: CommandOptions;
+}
+
+export type Permission = 'broadcaster' | 'moderator' | 'subscriber'
+/**
+ *Command handler class for managing chat commands 
+ * 
+ */
+export class CommandHandler {
+    private commands: Map<string, Command>
+
+    /**
+     * Create a new command handler
+     */
+    constructor() {
+        this.commands = new Map();
+    }
+
+    /**
+     * Register a new command
+     * @param commandName - Command name (without prefix)
+     * @returns success status
+     */
+    register(commandName: string, handler: CommandHandler, options: CommandOptions) {
+
+        if (this.commands.get(commandName) !== undefined) {
+            //TODO: log this1
+            return false
+        }
+
+        if (typeof handler !== "function") {
+            return false
+        }
+
+        this.commands.set(commandName, {
+            handler,
+            options: {
+                description: options.description || "no description",
+                //TODO: add functionality to create specific permissions as a default
+                requiredPermissions: options.requiredPermissions || ["broadcaster"],
+                approvedUsers: options.approvedUsers || [],
+                notifyDenial: options.notifyDenial || false,
+                usage: options.usage || "",
+                timeLimit: options.timeLimit || -1
+            }
+        })
+
+        return true;
+    }
+
+
+}
