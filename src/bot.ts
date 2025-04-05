3c490b21ecb529e23f05d70136cf4e0000d69bc4
Hunter Lewis <thesupprter@gmail.com>
2025-04-05 03:41:54 +0000

    Add Prettier configuration and update dependencies
    - Created a `.prettierignore` file to exclude specific directories and files from formatting.
    - Updated `package.json` and `package-lock.json` to include Prettier and ESLint dependencies.
    - Modified `index.ts` and `bot.ts` for consistent formatting and improved readability.
    - Adjusted CI workflow to use Node.js version 22.x only.
    - Added VSCode settings for automatic formatting on save.

--- src/bot.ts
+++ src/bot.ts
@@ -1,22 +1,25 @@
-import * as tmi from 'tmi.js'
-import { CommandHandler, CommandHandlerFunction } from './commands';
+import * as tmi from "tmi.js";
+import { CommandHandler, CommandHandlerFunction } from "./commands";
 
 export interface BotOptions {
-    identity: {
-        username: string;
-        password: string;
-    };
-    channels: string[];
-    debug?: boolean;
-    commandPrefix?: string;
+  identity: {
+    username: string;
+    password: string;
+  };
+  channels: string[];
+  debug?: boolean;
+  commandPrefix?: string;
 }
 
-
 export interface BotInterface {
-    client: tmi.Client,
-    connect(): Promise<Boolean>
-    disconnect(): Promise<Boolean>
-    registerCommand(commandName: string, handler: CommandHandlerFunction, options?: any): boolean
+  client: tmi.Client;
+  connect(): Promise<Boolean>;
+  disconnect(): Promise<Boolean>;
+  registerCommand(
+    commandName: string,
+    handler: CommandHandlerFunction,
+    options?: any,
+  ): boolean;
 }
 
 /**
@@ -24,60 +27,71 @@ export interface BotInterface {
  */
 
 export function createBot(options: BotOptions): BotInterface {
+  const commandHandler = new CommandHandler({
+    prefix: options.commandPrefix || "!",
+  });
 
-    const commandHandler = new CommandHandler({ prefix: options.commandPrefix || "!" })
+  const client = new tmi.Client({
+    identity: options.identity,
+    channels: options.channels,
+    options: { debug: options.debug },
+    connection: {
+      secure: true,
+      reconnect: true,
+    },
+  });
 
-    const client = new tmi.Client({
-        identity: options.identity,
-        channels: options.channels,
-        options: { debug: options.debug },
-        connection: {
-            secure: true,
-            reconnect: true
-        }
-    });
+  client.on(
+    "message",
+    (
+      channel: string,
+      tags: tmi.ChatUserstate,
+      message: string,
+      self: boolean,
+    ) => {
+      //ignore messages from itself (the bot)
+      if (self) return;
 
+      if (message.startsWith(options.commandPrefix || "!")) {
+        // this is redundant, on purpose
+        commandHandler.processCommand(client, channel, message, tags);
+      }
+    },
+  );
 
-    client.on('message', (channel: string, tags: tmi.ChatUserstate, message: string, self: boolean) => {
-        //ignore messages from itself (the bot)
-        if (self) return;
+  const botInterface: BotInterface = {
+    client,
+    connect: async (): Promise<Boolean> => {
+      //Potential bug, if connection doesn't work, then it's probably here
+      try {
+        await client.connect();
+        return true;
+      } catch (error) {
+        //TODO: Make a logger throw an error
+        console.log("Failed to connect to twitch %d", error);
+        throw error;
+      }
+    },
 
-        if (message.startsWith(options.commandPrefix || "!")) { // this is redundant, on purpose
-            commandHandler.processCommand(client, channel, message, tags)
-        }
+    disconnect: async (): Promise<Boolean> => {
+      //Potential bug, if connection doesn't work, then it's probably here
+      try {
+        await client.disconnect();
+        return true;
+      } catch (error) {
+        //TODO: Make a logger throw an error
+        console.log("Failed to disconnect to twitch %d", error);
+        throw error;
+      }
+    },
 
-
-    })
-
-    const botInterface: BotInterface = {
-        client,
-        connect: async (): Promise<Boolean> => {
-            //Potential bug, if connection doesn't work, then it's probably here
-            try {
-                await client.connect()
-                return true
-            } catch (error) {
-                //TODO: Make a logger throw an error
-                console.log("Failed to connect to twitch %d", error)
-                throw error
-            }
-        },
-
-        disconnect: async (): Promise<Boolean> => {
-            //Potential bug, if connection doesn't work, then it's probably here
-            try {
-                await client.disconnect()
-                return true
-            } catch (error) {
-                //TODO: Make a logger throw an error
-                console.log("Failed to disconnect to twitch %d", error)
-                throw error
-            }
-        },
-
-        registerCommand: (commandName: string, handler: CommandHandlerFunction, options?: any): boolean => {
-            return commandHandler.registerCommand(commandName, handler, options)
-        },
-    }
-    return botInterface;
+    registerCommand: (
+      commandName: string,
+      handler: CommandHandlerFunction,
+      options?: any,
+    ): boolean => {
+      return commandHandler.registerCommand(commandName, handler, options);
+    },
+  };
+  return botInterface;
 }
--- src/commands.ts
+++ src/commands.ts
@@ -5,151 +5,165 @@
 //We need permissions just to see if they're the broadcaster, mod, sub, vip, etc, or specific users
 //Getters
 
-import * as tmi from "tmi.js"
-const logger = require("./utils/logger")
+import * as tmi from "tmi.js";
+const logger = require("./utils/logger");
 
 export interface CommandOptions {
-    description?: string;
-    requiredPermissions?: Permission[];
-    approvedUsers?: string[];
-    notifyDenial?: boolean;
-    usage?: string;
-    timeLimit?: number;
-
+  description?: string;
+  requiredPermissions?: Permission[];
+  approvedUsers?: string[];
+  notifyDenial?: boolean;
+  usage?: string;
+  timeLimit?: number;
 }
 
 export type CommandHandlerFunction = (
-    client: tmi.Client,
-    channel: string,
-    tags: tmi.ChatUserstate,
-    args: string[]
+  client: tmi.Client,
+  channel: string,
+  tags: tmi.ChatUserstate,
+  args: string[],
 ) => void;
 
 export interface CommandHandlerOptions {
-    prefix: string;
-    requiredPermissions?: Permission[];
+  prefix: string;
+  requiredPermissions?: Permission[];
 }
 
 export interface Command {
-    commandFunction: CommandHandlerFunction;
-    options: CommandOptions;
+  commandFunction: CommandHandlerFunction;
+  options: CommandOptions;
 }
 
-export type Permission = 'broadcaster' | 'moderator' | 'subscriber'
+export type Permission = "broadcaster" | "moderator" | "subscriber";
 /**
- *Command handler class for managing chat commands 
- * 
+ *Command handler class for managing chat commands
+ *
  */
 export class CommandHandler {
+  private commands: Map<string, Command>;
+  public options: CommandHandlerOptions;
 
-    private commands: Map<string, Command>;
-    public options: CommandHandlerOptions;
+  /**
+   * Create a new command handler
+   */
+  constructor(options: CommandHandlerOptions) {
+    this.commands = new Map();
+    this.options = {
+      prefix: options.prefix,
+      requiredPermissions: options.requiredPermissions,
+    };
+    //TODO: default required Permissions
+  }
 
-    /**
-     * Create a new command handler
-     */
-    constructor(options: CommandHandlerOptions) {
-        this.commands = new Map();
-        this.options = {
-            prefix: options.prefix,
-            requiredPermissions: options.requiredPermissions
-        };
-        //TODO: default required Permissions
+  /**
+   * Register a new command
+   * @param commandName - Command name (without prefix)
+   * @param handler - Command handler function
+   * @param options - Command options
+   * @returns success status
+   */
+  registerCommand(
+    commandName: string,
+    commandFunction: CommandHandlerFunction,
+    options: CommandOptions,
+  ): boolean {
+    if (this.commands.get(commandName) !== undefined) {
+      //TODO: utilize logger
+      console.log("Cannot be an existing command");
+      return false;
     }
 
-    /**
-     * Register a new command
-     * @param commandName - Command name (without prefix)
-     * @param handler - Command handler function
-     * @param options - Command options
-     * @returns success status
-     */
-    registerCommand(commandName: string, commandFunction: CommandHandlerFunction, options: CommandOptions): boolean {
-
-        if (this.commands.get(commandName) !== undefined) {
-            //TODO: utilize logger
-            console.log("Cannot be an existing command")
-            return false
-        }
-
-        if (typeof commandFunction !== "function") {
-            console.log("Handler function needs to be a function")
-            return false
-        }
-
-        this.commands.set(commandName.toLowerCase(), {
-            commandFunction,
-            options: {
-                description: options.description || "no description",
-                //TODO: add functionality to create specific permissions as a default
-                requiredPermissions: options.requiredPermissions || ["broadcaster"],
-                approvedUsers: options.approvedUsers || [],
-                notifyDenial: options.notifyDenial || false,
-                usage: options.usage || "",
-                timeLimit: options.timeLimit || -1
-            }
-        });
-        return true;
+    if (typeof commandFunction !== "function") {
+      console.log("Handler function needs to be a function");
+      return false;
     }
 
+    this.commands.set(commandName.toLowerCase(), {
+      commandFunction,
+      options: {
+        description: options.description || "no description",
+        //TODO: add functionality to create specific permissions as a default
+        requiredPermissions: options.requiredPermissions || ["broadcaster"],
+        approvedUsers: options.approvedUsers || [],
+        notifyDenial: options.notifyDenial || false,
+        usage: options.usage || "",
+        timeLimit: options.timeLimit || -1,
+      },
+    });
+    return true;
+  }
 
-    /**
-     * Register a new command
-     * @param client - tmi client
-     * @param channel - the channel the bot will run the command in
-     * @param message - the chat message that called the command
-     * @param tags - the chatters tags which determine permissions
-     * @returns success status
-     */
-    processCommand(client: tmi.Client, channel: string, message: string, tags: tmi.ChatUserstate): boolean {
-        if (!message.startsWith(this.options.prefix)) {
-            return false
-        }
-
-        // extract command and arguments
-        const partsOfCommand = message.slice(this.options.prefix.length).split(/\s+/)
-        const commandName = partsOfCommand[0].toLowerCase() // command name is only the first element
-        const args = partsOfCommand.slice(1)
-
-        if (!this.commands.has(commandName)) {
-            client.say(channel, "Invalid Command")
-            return false
-        }
-
-        if (!this.checkPermissions(tags, this.options.requiredPermissions || ["broadcaster"])) {
-            client.say(channel, "Permission Denied")
-            return false
-        }
-
-        const command = this.commands.get(commandName) // retrieves command (includes handler and options)
-        if (command === undefined) return false;
-
-        try {
-            command.commandFunction(client, channel, tags, args)
-            return true;
-        } catch (error) {
-            console.log("An error occurred while executing the command: %d", error)
-            return true;
-        }
-
+  /**
+   * Register a new command
+   * @param client - tmi client
+   * @param channel - the channel the bot will run the command in
+   * @param message - the chat message that called the command
+   * @param tags - the chatters tags which determine permissions
+   * @returns success status
+   */
+  processCommand(
+    client: tmi.Client,
+    channel: string,
+    message: string,
+    tags: tmi.ChatUserstate,
+  ): boolean {
+    if (!message.startsWith(this.options.prefix)) {
+      return false;
     }
 
+    // extract command and arguments
+    const partsOfCommand = message
+      .slice(this.options.prefix.length)
+      .split(/\s+/);
+    const commandName = partsOfCommand[0].toLowerCase(); // command name is only the first element
+    const args = partsOfCommand.slice(1);
 
-    checkPermissions(tags: tmi.ChatUserstate, requiredPermissions: Permission[]): boolean {
-        if (!requiredPermissions || requiredPermissions.length == 0) { // checks if requiredPermissions exists or if it has no requirements
-            return true
-        }
-        return requiredPermissions.some(permission => {
-            switch (permission) {
-                case "broadcaster":
-                    return (tags.badges?.broadcaster)
-                case "moderator":
-                    return (tags.badges?.moderator)
-                case "subscriber":
-                    return (tags.badges?.subscriber)
-                default:
-                    return false
-            }
-        })
+    if (!this.commands.has(commandName)) {
+      client.say(channel, "Invalid Command");
+      return false;
     }
+
+    if (
+      !this.checkPermissions(
+        tags,
+        this.options.requiredPermissions || ["broadcaster"],
+      )
+    ) {
+      client.say(channel, "Permission Denied");
+      return false;
+    }
+
+    const command = this.commands.get(commandName); // retrieves command (includes handler and options)
+    if (command === undefined) return false;
+
+    try {
+      command.commandFunction(client, channel, tags, args);
+      return true;
+    } catch (error) {
+      console.log("An error occurred while executing the command: %d", error);
+      return true;
+    }
+  }
+
+  checkPermissions(
+    tags: tmi.ChatUserstate,
+    requiredPermissions: Permission[],
+  ): boolean {
+    if (!requiredPermissions || requiredPermissions.length == 0) {
+      // checks if requiredPermissions exists or if it has no requirements
+      return true;
+    }
+    return requiredPermissions.some((permission) => {
+      switch (permission) {
+        case "broadcaster":
+          return tags.badges?.broadcaster;
+        case "moderator":
+          return tags.badges?.moderator;
+        case "subscriber":
+          return tags.badges?.subscriber;
+        default:
+          return false;
+      }
+    });
+  }
 }
